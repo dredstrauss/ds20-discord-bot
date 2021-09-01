924feae2f68d5afbe8bc2609d7f60561b1bc9640
Dred Strauss <dred@dredstrauss.com>
2021-09-01 17:16:07 -0400

    Ping.js builders now relative

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('../node_modules/discord.js/node_modules/@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
