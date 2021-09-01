23772ab3c2b59136ab0a87a596289a57bf4414cf
Dred Strauss <dred@dredstrauss.com>
2021-09-01 17:34:07 -0400

    sintax fix ds.js

---
+++ commands/ds.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('../node_modules/discord.js/node_modules/@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ds')
+    .setDescription('Check if bot is running'),
+    async execute(interaction) {
+        await interaction.reply(`I'm here!`);
+    }
+};
