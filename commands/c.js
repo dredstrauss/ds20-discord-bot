c6db829727fc058f8a602df4e77b3e8812cdcbb6
Dred Strauss <dred@dredstrauss.com>
2021-09-01 21:17:48 -0400

    syntax fix in cjs

--- commands/c.js
+++ commands/c.js
@@ -4,7 +4,7 @@ const tirada = require('../Roll');
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('c')
-    .setDescription(`Tirada de característica: /c c[característica] b[bonificación] p[penalización]\nEjemplo: */c c13 b2 p1*`),
+    .setDescription(`Tirada de característica: /c c[característica] b[bonificación] p[penalización]`),
     async execute(interaction) {
         const input = message.content.slice(prefix.length).trim().split(/ +/g);
         const args = input.shift().toLowerCase();
@@ -30,40 +30,50 @@ module.exports = {
 
         const diceRolls = {
             d20 : parseInt(roll(20)),
-            arrBon : if (bon > 0) {
-                let arr = [];
-                for (let i = 0; i < bon; i++) { arr.push(roll(4)) };
-                return arr
-            } else {
-                return [0]
-            },
-            arrPen : if (pen > 0) {
-                let arr = [];
-                for (let i = 0; i < pen; i++) { arr.push(roll(4)) };
-                return arr
-            } else {
-                return [0]
-            },
+            arrBon : (function() {
+                if (bon > 0) {
+                    let arr = [];
+                    for (let i = 0; i < bon; i++) { arr.push(roll(4)) };
+                    return arr
+                } else {
+                    return [0]
+                }
+            }()),
+            arrPen : (function() {
+                if (pen > 0) {
+                    let arr = [];
+                    for (let i = 0; i < pen; i++) { arr.push(roll(4)) };
+                    return arr
+                } else {
+                    return [0]
+                }
+            }())
         };
 
         const margin = cha - diceRolls.d20 - diceRolls.arrBon.reduce((x,y)=>x+y) + diceRolls.arrPen.reduce((x,y)=>x+y);
 
         const message = {
-            bon : if (bon > 0) {
-                return ` - Bon.: **-${bon}**`
-            } else {
-                return ''
-            },
-            pen : if (pen > 0) {
-                return ` - Pen.: **+${pen}**`
-            } else {
-                return ''
-            },
-            result : if (margin < 0) {
-                return `\`\`\`diff\nMargen de fallo: **${margin}**\n\`\`\``
-            } else {
-                return `\`\`\`bash\nMargen de éxito: **${margin}**\n\`\`\``
-            }
+            bon : (function() {
+                if (bon > 0) {
+                    return ` - Bon.: **-${bon}**`
+                } else {
+                    return ''
+                }
+            }()),
+            pen : (function() {
+                if (pen > 0) {
+                    return ` - Pen.: **+${pen}**`
+                } else {
+                    return ''
+                }
+            }()),
+            result : (function() {
+                if (margin < 0) {
+                    return `\`\`\`diff\nMargen de fallo: **${margin}**\n\`\`\``
+                } else {
+                    return `\`\`\`bash\nMargen de éxito: **${margin}**\n\`\`\``
+                }
+            }())
         }
 
         await interaction.reply(
