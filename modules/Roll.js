ac28d0c77c990edf06db39797d33207ced930339
Dred Strauss <dred@dredstrauss.com>
2021-09-04 14:09:44 -0400

    between function added to roll.js

--- modules/Roll.js
+++ modules/Roll.js
@@ -1,7 +1,7 @@
+const between = require('./Between');
+
 const roll = (arg) => {
-    const faces = parseInt(arg);
-    if (faces < 1) { faces = 1 };
-    if (faces > 999) { faces = 999 };
+    const faces = between(parseInt(arg),1,99);
     let result = Math.floor( (Math.random() * faces ) +1 );
     return result
 }
