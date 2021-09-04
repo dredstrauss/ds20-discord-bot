56812860767e2696d1bcccc063e2ff36dec41ca2
Dred Strauss <dred@dredstrauss.com>
2021-09-04 11:35:17 -0400

    language support for d.js

--- commands/d.js
+++ commands/d.js
@@ -3,46 +3,49 @@ const { SlashCommandBuilder } = require('../node_modules/discord.js/node_modules
 const roll = require('../modules/Roll');
 const between = require('../modules/Between');
 
+const lang = process.env.LANGUAGE;
+const text = require('../lang.json');
+
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('d')
-    .setDescription(`Tirada de dados genérica`)
+    .setName(text.d.name[lang])
+    .setDescription(text.d.description[lang])
     .addStringOption(option =>
-        option.setName('caras')
-        .setDescription('Caras del dado (ej.: 4, 6, 8, 10, 12, 20, 100)')
+        option.setName(text.d.opt1Name[lang])
+        .setDescription(text.d.opt1Description[lang])
         .setRequired(true)
     )
     .addStringOption(option =>
-        option.setName('cantidad')
-        .setDescription('Cantidad de dados similares a lanzar (0 o más)')
+        option.setName(text.d.opt2Name[lang])
+        .setDescription(text.d.opt2Description[lang])
         .setRequired(false)
     ),
     async execute(interaction) {
 
-        let caras = between(interaction.options.getString('caras'),1,999);
-        let cantidadInput = interaction.options.getString('cantidad');
-        let cantidad = 1;
+        let sides = between(interaction.options.getString(text.d.opt1Name[lang]),1,999);
+        let sidesInput = interaction.options.getString(text.d.opt2Name[lang]);
+        let dice = 1;
 
-        if (parseInt(cantidadInput) > 1) { cantidad = between(cantidadInput,1,99) };
+        if (parseInt(sidesInput) > 1) { dice = between(sidesInput,1,99) };
 
         let results = [];
 
-        for (let i = 0; i < cantidad; i++) results.push(roll(caras));
+        for (let i = 0; i < dice; i++) results.push(roll(sides));
 
         let messages = {
-            dieOrDice : 'Dado',
-            results : 'Resultado',
+            dieOrDice : text.d.message1[lang],
+            results : text.d.message2[lang],
             sum : ''
         }
 
-        if (cantidad > 1) {
-            messages.dieOrDice = 'Dados';
-            messages.results = 'Resultados';
-            messages.sum = `*(Suma de las tiradas: ${results.reduce((x,y)=>x+y)})*`;
+        if (dice > 1) {
+            messages.dieOrDice = text.d.message3[lang];
+            messages.results = text.d.message4[lang];
+            messages.sum = `*(${text.d.message5[lang]}: ${results.reduce((x,y)=>x+y)})*`;
         };
 
         await interaction.reply(
-            `${messages.dieOrDice}: ${cantidad}d${caras}\n`+
+            `${messages.dieOrDice}: ${dice}d${sides}\n`+
             `>>> ${messages.results}: **${results}**\n`+
             `${messages.sum}`
         );
